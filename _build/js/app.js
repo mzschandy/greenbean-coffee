c87bb557f2e0dcea3f900be2cd7584b5cd6eaf85
Zach SL <mzschandy@gmail.com>
2021-01-24 19:48:05 -0500

    finished home layout

--- _build/js/app.js
+++ _build/js/app.js
@@ -1,1 +1,24 @@
-$(document).foundation();
+//$(document).foundation();
+
+window.onscroll = function() {
+    navScroll()
+}
+
+function navScroll() {
+    if (document.body.scrollTop > 141 || document.documentElement.scrollTop > 80) {
+        console.log("change");
+        $("#header").addClass("scroll");
+    } else {
+        console.log("change back");
+        $("#header").removeClass("scroll");
+    }
+}
+
+$(document).ready(function() {
+    $(".menuBtn").click(function() {
+        $(".mobile").css("height", "100%")
+    })
+    $(".closeBtn").click(function() {
+        $(".mobile").css("height", "0%")
+    })
+})
