518a9b82228a14a3e1e470e077e1739aad7c92f2
Zach SL <mzschandy@gmail.com>
2021-10-21 07:43:13 -0400

    added animations

--- _build/js/app.js
+++ _build/js/app.js
@@ -22,3 +22,43 @@ $(document).ready(function() {
         $(".mobile").css("height", "0%")
     })
 })
+
+/*
+const callback = function(entries) {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("animate__animated", "animate__fadeInUp");
+        entry.target.classList.toggle(".isVisible");
+    })
+*/
+
+const options = {
+    rootMargin: "32px",
+    threshold: 0
+}
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+            entry.target.classList.add("animate__animated", "animate__fadeInUp");
+        }
+    })
+}, options);
+
+const observer2 = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+            entry.target.classList.add("animate__animated", "animate__fadeInLeft");
+        }
+    })
+}, options);
+
+const targets = document.querySelectorAll(".animated");
+const imageTargets = document.querySelectorAll(".animatedImage");
+
+
+targets.forEach( (target) => {
+    observer.observe(target);
+})
+imageTargets.forEach( (target) => {
+    observer2.observe(target);
+})
